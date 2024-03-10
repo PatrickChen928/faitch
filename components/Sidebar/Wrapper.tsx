1216d3ae0b335d15d97f2d9754a768b19704052c
patrick <786156072@qq.com>
2024-03-10 10:37:23 +0800

    fix: useIsClient

--- components/Sidebar/Wrapper.tsx
+++ components/Sidebar/Wrapper.tsx
@@ -1,20 +1,16 @@
 'use client'
 
-import { useEffect, useState } from "react"
 import { cn } from "@/lib/utils"
 import { useSidebar } from "@/store/user-sidebar"
 import { ToggleSkeleton } from "./Toggle"
 import { RecommendedSkeleton } from "./Recommended"
+import { useIsClient } from "usehooks-ts"
 
 export default function Wrapper({ children }: {
   children: React.ReactNode
 }) {
-  const [isClient, setIsClient] = useState(false)
+  const isClient = useIsClient()
   const { isOpen } = useSidebar((state) => state)
-
-  useEffect(() => {
-    setIsClient(true)
-  }, [])
 
   if (!isClient) return (
     <aside className="fixed left-0 flex w-[70px] lg:w-60 flex-col h-full bg-sidebar border-r border-[#2D2E35] z-50">
