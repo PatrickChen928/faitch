c2124efc8526dd787fa8e8af53afc0c512ab2188
patrick <786156072@qq.com>
2024-03-12 20:56:18 +0800

    WIP: keys page

--- app/(dashboard)/u/[username]/keys/page.tsx
+++ app/(dashboard)/u/[username]/keys/page.tsx
@@ -1,6 +1,24 @@
+"use client"
+
+import { useEffect } from "react";
+import { useGetStreamByUserId } from "@/lib/react-query/stream";
 import { Button } from "@/components/ui/button";
+import { useUser } from "@/components/UserContext";
+import KeyCard from "./_components/KeyCard";
+import UrlCard from "./_components/UrlCard";
 
 export default function KeysPage() {
+
+  const { current } = useUser();
+
+  const { data, mutateAsync } = useGetStreamByUserId()
+
+  useEffect(() => {
+    if (current) {
+      mutateAsync(current.$id)
+    }
+  }, [current])
+
   return (
     <div className="p-6">
       <div className="flex items-center justify-between mb-4">
@@ -8,6 +26,10 @@ export default function KeysPage() {
         <Button variant="primary">
           Generate
         </Button>
+      </div>
+      <div className="space-y-4">
+        <UrlCard value={data?.serverUrl} />
+        <KeyCard value={data?.streamKey} />
       </div>
     </div>
   );
