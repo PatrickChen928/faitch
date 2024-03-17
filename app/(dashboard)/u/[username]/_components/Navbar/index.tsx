e71cb2d4fdc825861e482e43fdead63d0120fd25
patrick <786156072@qq.com>
2024-03-17 11:10:34 +0800

    fix: style

--- app/(dashboard)/u/[username]/_components/Navbar/index.tsx
+++ app/(dashboard)/u/[username]/_components/Navbar/index.tsx
@@ -6,8 +6,8 @@ import GithubLink from "@/components/GithubLink";
 export default function Navbar() {
   return (
     <nav className="fixed top-0 w-full h-20 z-[49] bg-nav px-2 lg:px-4 flex justify-between items-center shadow-sm gap-4">
-      <div className="flex items-center gap-x-2">
-        <Link href="/">
+      <div className="flex-shrink-0 flex items-center gap-x-2">
+        <Link href="/" className="flex-shrink-0">
           <div className="flex items-center hover:opacity-75 transition">
             <Logo hiddenTitle={true} />
           </div>
